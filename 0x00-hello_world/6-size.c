1f92f6c8e8fc8d6a0ff3a963959181a2a8560ec1
Onederrrful <hope84bala@gmail.com>
2023-04-20 21:18:53 -0700

    6-size.c

--- 0x00-hello_world/6-size.c
+++ 0x00-hello_world/6-size.c
@@ -1,8 +1,7 @@
-#include <stdio.h>                                                                                                                      
-/** 
- *  * main - Entry
- *   * Return: Always 0
- *    */
+#include <stdio.h>                                           /** 
+ * * main - Entry
+ *  * Return: Always 0
+ *  */
 main(void)
 	int main(void)
 {
