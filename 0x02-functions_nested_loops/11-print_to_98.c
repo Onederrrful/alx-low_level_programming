1485fd7c364003b87eacfc0da3ec5db52d982222
Onederrrful <hope84bala@gmail.com>
2023-04-25 10:27:02 -0700

    11-print_to_98.c

--- 0x02-functions_nested_loops/11-print_to_98.c
+++ 0x02-functions_nested_loops/11-print_to_98.c
@@ -1,47 +1,26 @@
+#include <stdio.h>
 #include "main.h"
 
 /**
- * print_times_table - prints the n times table, starting with 0
- * @n: number of the times table
+ * print_to_98 - prints all natural numbers from n to 98
+ * @n: the input
  */
-void print_times_table(int n)
-{
-	int i, j, k;
 
-	if (n >= 0 && n <= 15)
+void print_to_98(int n)
+{
+	int x = 98;
+
+	if (n >= x)
 	{
-		for (i = 0; i <= n; i++)
-		{
-			for (j = 0; j <= n; j++)
-			{
-				k = j * i;
-				if (j == 0)
-				{
-					_putchar(k + '0');
-				} else if (k < 10 && j != 0)
-				{
-					_putchar(',');
-					_putchar(' ');
-					_putchar(' ');
-					_putchar(' ');
-					_putchar(k + '0');
-				} else if (k >= 10 && k < 100)
-				{
-					_putchar(',');
-					_putchar(' ');
-					_putchar(' ');
-					_putchar((k / 10) + '0');
-					_putchar((k % 10) + '0');
-				} else if (k >= 100)
-				{
-					_putchar(',');
-					_putchar(' ');
-					_putchar((k / 100) + '0');
-					_putchar(((k / 10) % 10) + '0');
-					_putchar((k % 10) + '0');
-				}
-			}
-			_putchar('\n');
-		}
+		while (n > x)
+			printf("%d, ", n--);
+		printf("%d\n", n);
 	}
+	else
+	{
+		while (n < x)
+			printf("%d, ", n++);
+		printf("%d\n", n);
+	}
+
 }
