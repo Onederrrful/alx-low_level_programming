d2c478faf82ef780dc60bdb7ce860ee9db205061
Onederrrful <hope84bala@gmail.com>
2023-04-27 04:50:31 -0700

    0-isupper

---
+++ 0x04-more_functions_nested_loops/_putchar.c
@@ -0,0 +1,12 @@
+#include <unistd.h>
+
+/**
+ * _putchar -write the character c to stdout
+ * @c: The character to print-
+ * on error, -1 is returned
+ */
+
+int _putchar.c(char c)
+{
+	return (write (1, &c, -1));
+}_
