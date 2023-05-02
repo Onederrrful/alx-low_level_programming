22badbf766103ac5c3b5e9585c6959975a2f9fd8
Onederrrful <hope84bala@gmail.com>
2023-05-02 07:49:28 -0700

    2-strlen.c

--- 0x05-pointers_arrays_strings/2-strlen.c
+++ 0x05-pointers_arrays_strings/2-strlen.c
@@ -1,22 +1,17 @@
 #include "main.h"
-
 /**
- * _puts - prints a string, followed by a new line.
- * @str: input string.
- * Return: no return.
+ *_strlen-Finds the length of a string.
+ *@s:String pointer to the string whose length is to be found.
+ *Return: returns the length of the string.
  */
-void _puts(char *str)
-{
-	int count = 0;
 
-	while (count >= 0)
+int _strlen(char *s)
+{
+	int p = 0;
+	/*incremeant up to when the last character is NULL,\0*/
+	while (*(s + p) != 0)
 	{
-		if (str[count] == '\0')
-		{
-			_putchar('\n');
-			break;
-		}
-		_putchar(str[count]);
-		count++;
+		p++;
 	}
+	return (p);
 }
