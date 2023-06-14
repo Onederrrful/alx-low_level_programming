98a73de56af0368cbdf38e49e91416a010b2680e
Onederrrful <hope84bala@gmail.coom>
2023-06-13 21:29:10 -0700

    0x15. C - File I/O

--- 0x15-file_io/1-create_file.c
+++ 0x15-file_io/1-create_file.c
@@ -1,49 +1,27 @@
-#include <unistd.h>
-#include <stdlib.h>
-#include <fcntl.h>
 #include "main.h"
-
 /**
- * create_file - append to file
- * @filename: path to file
- * @text_content: content
- * Return: chars read 1 on success -1 om failure
- */
-
+* create_file - Creates a file.
+* @filename: points to the name of the file to create.
+* @text_content: A pointer to a string to write to the file.
+*
+* Return: If the function fails - -1.
+* Otherwise - 1.
+*/
 int create_file(const char *filename, char *text_content)
 {
-	int fd;
-	ssize_t w;
-	int size;
-	char *mem;
-
-	if (!filename)
-	{
-		return (-1);
-	}
-	fd = open(filename, O_RDWR | O_CREAT | O_TRUNC, 0600);
-	if (fd == -1)
-		return (-1);
-	if (!text_content)
-	{
-		close(fd);
-		return (1);
-	}
-	size = _strlen(text_content);
-	mem = malloc(sizeof(char) * size);
-	if (!mem)
-	{
-		close(fd);
-		return (-1);
-	}
-	w = write(fd, text_content, size);
-	if (w == -1)
-	{
-		close(fd);
-		free(mem);
-		return (-1);
-	}
-	close(fd);
-	free(mem);
-	return (1);
+int fd, w, len = 0;
+if (filename == NULL)
+return (-1);
+if (text_content != NULL)
+{
+for (len = 0; text_content[len];)
+len++;
 }
+fd = open(filename, O_CREAT | O_RDWR | O_TRUNC, 0600);
+w = write(fd, text_content, len);
+if (fd == -1 || w == -1)
+return (-1);
+close(fd);
+return (1);
+}
+
